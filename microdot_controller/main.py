9cf46f93bfa1b290fd16bb2c069d6f57c19f3750
lmkawakami <lmkawakami@hotmail.com>
2025-05-30 01:07:06 -0300

    Implement WebSocket support and refactor LED control routes

--- microdot_controller/main.py
+++ microdot_controller/main.py
@@ -2,6 +2,7 @@ print("Microdot Controller is running...")
 from wifi_utils import connect_to_wifi
 from machine import Pin
 from microdot import Microdot, send_file
+from microdot.websocket import with_websocket
 import time
 
 LED = Pin(15, Pin.OUT)    # create output pin on GPIO0
@@ -13,30 +14,19 @@ app = Microdot()
 async def index(request):
     return 'Hello, world!'
 
+@app.route('/status')
+@with_websocket
+async def status(request, ws):
+    while True:
+        message = await ws.receive()
+        await ws.send(message)
+
 @app.route('/public/<path:path>')
 async def public(request, path):
     if '..' in path:
         # directory traversal is not allowed
         return 'Not found', 404
     return send_file('public/' + path, max_age=86400)
-
-@app.route('/led_toggle')
-async def led_toggle(request):
-    LED.value(not LED.value())
-    print("led toggled")
-    return 'OK'
-
-@app.route('/led_on')
-async def led_on(request):
-    LED.value(0)
-    print("led on")
-    return 'OK - ON'
-
-@app.route('/led_off')
-async def led_off(request):
-    LED.value(1)
-    print("led off")
-    return 'OK - OFF'
 
 if connect_to_wifi():
     print("Connected to WiFi")
