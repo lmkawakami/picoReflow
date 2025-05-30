32888c06779775cfe55135891501abb93a159a5a
lmkawakami <lmkawakami@hotmail.com>
2025-05-29 22:03:22 -0300

    Refactor KilnControllerHandler to remove unused imports and simplify WebSocket integration

--- esp32-controller/kiln_controller/kiln_controller_handler.py
+++ esp32-controller/kiln_controller/kiln_controller_handler.py
@@ -1,20 +1,9 @@
-import esp32
-from filemanager.web_server import (
-    FM_200_JSON,
-    FM_200_TEXT,
-)
-from filemanager.web_server_plugin import (
-    WebServerPlugin
-)
+import uasyncio as asyncio
 import json
 
 
-class KilnControllerHandler(WebServerPlugin):
-    @staticmethod
-    @WebServerPlugin.handler('/status')
-    def get_controller_status(client, path, request):
-        response = {
-            "Board temperature": "OK"
-        }
-        client.send(FM_200_JSON)
-        client.send(json.dumps(response))
+class KilnControllerHandler:
+    def __init__(self, webserver):
+        self.webserver = webserver
+        # Registra a rota /status para conexões WebSocket
+        self.webserver.add_websocket_route('/status', self.websocket_handler)
