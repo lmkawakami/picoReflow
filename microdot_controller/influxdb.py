4b6bb3ee7bd89ae9f19002be1d29ea06fcc1d43b
lmkawakami <lmkawakami@hotmail.com>
2025-06-02 21:49:37 -0300

    Refactor InfluxDB class as singleton

--- microdot_controller/influxdb.py
+++ microdot_controller/influxdb.py
@@ -1,10 +1,24 @@
 import requests
 import aiohttp
+from singleton import singleton
+
+
+@singleton
 class InfluxDB:
-    def __init__(self, base_url, api_token, organization, bucket, instance_name):
+    instance_name: str
+    url: str
+    headers: dict
+    configured: bool
+
+    def __init__(self):
+        self.configured = False
+
+    def config(self, base_url, api_token, organization, bucket, instance_name):
         self.instance_name = instance_name
         self.url = f"{base_url}/api/v2/write?org={organization}&bucket={bucket}&precision=s"
         self.headers = {"Authorization": f"Token {api_token}"}
+        self.configured = True
+
 
     def write(self, fields: dict, tags: dict, timestamp: int):
         try:
@@ -33,7 +47,7 @@ class InfluxDB:
         return ",".join(f'{k}={v}' for k, v in dict_data.items())
 
     def _format_data(self, fields: dict, tags: dict, timestamp: int) -> str:
-        tag_set = InfluxDB._format(tags)
-        field_set = InfluxDB._format(fields)
+        tag_set = self._format(tags)
+        field_set = self._format(fields)
         data = f"{self.instance_name},{tag_set} {field_set} {timestamp}"
         return data
